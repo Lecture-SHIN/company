a972f324a275b806ff4639c286ed09fd927949ef
loophyun <dhkim960511@naver.com>
2024-12-05 10:19:42 +0900

    add feature: infoKdh1 - CommonController

---
+++ src/main/java/app/api/controller/CommonController.java
@@ -0,0 +1,13 @@
+package app.api.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CommonController {
+	
+	@GetMapping("/kdh1")
+	public String infoKdh1() {
+		return "kdh";
+	}
+}
